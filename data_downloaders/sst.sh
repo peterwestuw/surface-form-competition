e01b427a574b873a10baf4899717ad606e73d71d
Ari Holtzman <20871523+ari-holtzman@users.noreply.github.com>
2021-09-07 17:16:16 -0700

    include test set for SST-2

--- data_downloaders/sst.sh
+++ data_downloaders/sst.sh
@@ -1,7 +1,8 @@
 mkdir data/sst-2/
 mkdir data/sst-5/
 cd data/sst-2/
-wget https://raw.githubusercontent.com/prrao87/fine-grained-sentiment/master/data/sst/sst_dev.txt -O dev.txt
-wget https://raw.githubusercontent.com/prrao87/fine-grained-sentiment/master/data/sst/sst_test.txt -O test.txt
+wget https://raw.githubusercontent.com/prrao87/fine-grained-sentiment/master/data/sst/sst_dev.txt -O dev.tsv
+wget https://raw.githubusercontent.com/prrao87/fine-grained-sentiment/master/data/sst/sst_test.txt -O test.tsv
+wget https://raw.githubusercontent.com/prrao87/fine-grained-sentiment/master/data/sst/sst_train.txt -O train.tsv
 cd ../sst-5/
-cp ../sst-2/{dev,test}.txt .
+cp ../sst-2/{dev,test,train}.tsv .
